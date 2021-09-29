ad9a9ae988a9298f7d1b7f8d277c695e4ad92a2a
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-29 13:31:57 +0800

    Create Main.java

---
+++ Java Project/package_1/Main.java
@@ -0,0 +1,13 @@
+package package_1;
+
+import package_2.NewClass;
+
+public class Main {
+
+  public static void main(String[] args) {
+   
+   NewClass Obj = new NewClass();
+   
+   System.out.println(Obj.var1);
+  }
+}
